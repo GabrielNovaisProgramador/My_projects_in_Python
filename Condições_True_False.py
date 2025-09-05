71041fd0a589cca6f4dfc9610309971ebd41da93
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-04 21:14:08 -0300

    Fiz um exemplo com duas váriaveis armazenando valores inteiros diferentes. E com isso criei três condições para o programa gerar como resultado para mim, qual das condições é à verdadeira e quais são às falsas.

---
+++ Condições_True_False.py
@@ -0,0 +1,11 @@
+a = 3
+b = 5
+
+comparacao = (a == b)
+print("1°- a é igual à b: ", comparacao)
+
+comparacao = (a > b)
+print("2°- a é maior que b: ", comparacao)
+
+comparacao = (a <= b)
+print("3°- a é menor ou igual à b: ", comparacao)

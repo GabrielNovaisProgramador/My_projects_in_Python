0740deb979dfae3ce848f09c18caa5a08153e78b
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-04 21:52:04 -0300

    Outro exemplo de expressão lógica com o operador lógico (AND).

---
+++ Exemplo_com_AND.py
@@ -0,0 +1,10 @@
+salario = 937
+idade = 18
+
+condicao_um = (salario > 1000)
+condicao_dois = (idade > 18)
+condicao_tres = (salario > 1000 and idade > 18)
+
+print(condicao_um)
+print(condicao_dois)
+print(condicao_tres)

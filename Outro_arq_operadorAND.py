ed471f7abef394bb6255756972ae2865052f5cca
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-04 21:38:59 -0300

    Outro exemplo de expressão lógica com o operador lógico (AND).

---
+++ Outro_arq_operadorAND.py
@@ -0,0 +1,12 @@
+a = 10
+proposicao1 = (a > 1)
+proposicao2 = (a == 5)
+
+resultado = proposicao1 and proposicao2
+print(resultado)
+
+# Se você rodar esse programa, vai te gerar o resultado False.
+# Isso porque para gerar o operador lógico True, você precisa
+#satisfazer às todas às condições ou seja, tanto à "proposicao1"
+#como à "proposicao2" tem que ser verdadeiras, para o resultado
+#da expressão lógica ser True (verdadeira).

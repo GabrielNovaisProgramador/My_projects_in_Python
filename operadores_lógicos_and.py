1251ffa709609c54e5bfe40a24cd171e6cd52271
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-04 21:28:19 -0300

    Valores lógicos (True) e (False) com o operador lógico AND.

---
+++ operadores_lógicos_and.py
@@ -0,0 +1,14 @@
+a = True and True
+b = True and False
+c = False and True
+d = False and False
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Como podemos ver, se você executar essa impressão. Somente à condição "a"
+#vai resultar em (True). Porque com o operador AND, se tivermos dois valores
+#lógicos (False) à condição sempre será (False), o mesmo vai acontecer se à
+#condição for (False and True) ou (True and False).

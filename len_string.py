04cc457ef8f3401806dd756a2ce344b84a2c1944
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-09 00:38:40 -0300

    Usando o comando len para fazer à contagem de caractéres que faz parte de uma String na hora da impressão.

---
+++ len_string.py
@@ -0,0 +1,10 @@
+# Retornando o tamanho de uma string com len
+
+frase = "Aula de Python sobre strings!"
+tamanho_da_frase = len(frase)
+
+print(tamanho_da_frase)
+
+# Como vocês podem ver, assim que você colocar para executar o programa.
+# O mesmo vai te retornar à quantidade de caracteres que contém na
+# string que foi impressa.

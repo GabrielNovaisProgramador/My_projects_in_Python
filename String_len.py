baed266497a80ba9fafa38ebd06991e013b85715
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-10 22:47:29 -0300

    Usando o comando len para fazer à contagem de caractéres.

---
+++ String_len.py
@@ -0,0 +1,11 @@
+# À função "len" serve como um contador de caractere do tipo string,
+# onde você elabora um conteúdo do tipo string, armazena em uma variavel.
+# Cria outra variavel para passar à função "len",passando o nome da variavel
+# entre "()" que contém o conteúdo. E peça ao programa imprimir com o "print"
+# à variavel que está passando o contador.
+
+frase = "Aula de Python sobre strings!"
+tamanho_frase = len(frase)
+print(tamanho_frase)
+
+

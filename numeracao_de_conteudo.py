4f5e0d5356ad8e6a1721d967e2b5f7d7f106ea50
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-10 23:00:17 -0300

    Encontrando à posição de um conteúdo que está salvo na variavel, e solicitando ao programa para que ele nos retorne o caractere dessa posição.

---
+++ numeracao_de_conteudo.py
@@ -0,0 +1,8 @@
+curso = "PYTHON PARA TODOS!!!"
+
+print(curso[12])
+
+# Repara que se você rodar o programa, o mesmo vai te retornar o caractere "T"
+# isso porque junto com o comando "print" na linha 3, eu coloque o nome da
+# variavel que está armazenando o conteúdo e entre "[]" estou colocando à
+# posição 12 que se localiza dentro do conteúdo que está na variavel.

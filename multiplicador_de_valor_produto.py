22bdc7f52fa5c0ad920ad85e9673a2fc1dfbb684
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-11 21:15:00 -0300

    Programa multiplicador de valores de um produto em específico do quanto o consumidor vai ter que pagar.

---
+++ multiplicador_de_valor_produto.py
@@ -0,0 +1,9 @@
+# Abaixo criei um programa que solicita ao usuário, o valor de um produto
+# e quantos produtos ele quer comprar. Através do que ele passar ao programa
+# o programa retorna o valor de quanto ele vai pagar.
+
+valor_unitario = float(input("Informe o valor do produto que você "
+                             "quer comprar: "))
+quantidade = int(input("Informe a quantidade de quantos produtos desse "
+                       "você quer: "))
+print(f"O total da compra é: R${valor_unitario * quantidade}")

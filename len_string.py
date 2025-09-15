55a63bac92e2e2649aa9611025209088c48b9ead
Gabriel Novais Dev - Python <gnovaesmartins@gmail.com>
2025-09-15 20:46:44 -0300

    Update len_string.py

--- len_string.py
+++ len_string.py
@@ -5,6 +5,6 @@ tamanho_da_frase = len(frase)
 
 print(tamanho_da_frase)
 
-# Como vocês podem ver, assim que você colocar para executar o programa.
-# O mesmo vai te retornar à quantidade de caracteres que contém na
-# string que foi impressa.
+'''Como vocês podem ver, assim que você colocar para executar o programa.
+O mesmo vai te retornar à quantidade de caracteres que contém na
+string que foi impressa.'''

1cb8b30b05828ba7410a26829a1f73c1942be9ed
Gabriel Novais Dev - Python <gnovaesmartins@gmail.com>
2025-09-15 19:13:24 -0300

    Update Aprendendo_mais_de_strings.py
    Alterando apenas os sinais de comentarios no meu código.

--- Aprendendo_mais_de_strings.py
+++ Aprendendo_mais_de_strings.py
@@ -1,21 +1,21 @@
-# Abaixo temos uma concatenação entre strings, ou seja à união de dois
-# textos armazenados em váriaveis diferentes e junta em uma só mensagem.
+'''Abaixo temos uma concatenação entre strings, ou seja à união de dois
+textos armazenados em váriaveis diferentes e junta em uma só mensagem.'''
 
 texto1 = "Olá, "
 texto2 = "tudo bem? "
 texto_concatenado = texto1 + texto2
 print(texto_concatenado)
 
-# Abaixo você informa uma string, e o programa vai multiplicar essa string
-# por 10. Ou seja repetindo o texto por 10 vezes.
+''' Abaixo você informa uma string, e o programa vai multiplicar essa string
+por 10. Ou seja repetindo o texto por 10 vezes.'''
 
 alimento = "Pão "
 multiplica = alimento * 10
 print(multiplica)
 
-# Abaixo eu usei %s que exibi às strings armazenada em alguma váriavel
-# e %d para número inteiros. Tanto o %s como o %d segue uma sequência de
-# informações armazenadas na váriavel.
+''' Abaixo eu usei %s que exibi às strings armazenada em alguma váriavel
+e %d para número inteiros. Tanto o %s como o %d segue uma sequência de
+informações armazenadas na váriavel.'''
 
 nome = "João da Silva"
 idade = 38

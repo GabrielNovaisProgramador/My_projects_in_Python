7ce9abb973f191e254308670172f2e0b8b259165
Gabriel Novais Dev - Python <gnovaesmartins@gmail.com>
2025-09-15 20:50:14 -0300

    Update adicao_de_espaços.py

--- adicao_de_espaços.py
+++ adicao_de_espaços.py
@@ -1,13 +1,16 @@
-# No exemplo abaixo temos uma variavel que está armazenando o valor 9
-# e nos comandos prints abaixo estou informando o inteiro 9 junto com
-# a mensagem, onde o 9 está sendo acrescentado por zeros à esquerda,
-# espaços vazios e está entre colchetes para delimitar o espaço.
+'''No exemplo abaixo temos uma variavel que está armazenando o valor 9
+e nos comandos prints abaixo estou informando o inteiro 9 junto com
+a mensagem, onde o 9 está sendo acrescentado por zeros à esquerda,
+espaços vazios e está entre colchetes para delimitar o espaço.'''
+
 aula = 9
 print("Esta é à aula de número %03d" % aula)
 print("Esta é à aula de número %3d" % aula)
 print("Esta é à aula de número [%3d]" % aula)
 print("Esta é à aula de número [%03d]" % aula)
-# Repare que nessa última impressão, o programa coloca o inteiro entre colchetes
-# e três casas de espaços à direita.
+
+'''Repare que nessa última impressão, o programa coloca o inteiro entre colchetes
+e três casas de espaços à direita.'''
+
 print("Esta é à aula de número [%-3d]" % aula)
 

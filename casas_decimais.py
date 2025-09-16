66d83c0cf20cdae19c5a8b0a1c47322583884caa
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-15 23:54:35 -0300

    Exemplo de como exibir um número com uma ou mais casas decimais.

---
+++ casas_decimais.py
@@ -0,0 +1,9 @@
+'''Neste exemplo irei mostrar como faço para exibir 1,2,3 ou mais casas
+ decimais com o número 165.78 de exemplo'''
+
+exemplo = 165.78
+print("%f" % exemplo)
+print("%.f" % exemplo)
+print("%.1f" % exemplo)
+print("%.2f" % exemplo)
+print("%.3f" % exemplo)

def02a8ee28f63b837336d317df348e73f34286c
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2025-09-17 00:38:59 -0300

    Exemplo de como exibir posições de caracteres de uma string.

---
+++ selecao_exibicao.py
@@ -0,0 +1,33 @@
+email = "devpython@gmail.com"
+indice = email.index(".com")
+print(indice)# Vai imprimir à posição 15
+
+indice = email.index("@gmail")
+print(indice)# Será impresso à posição 9 do nosso gmail.
+
+indice = email.index("@")
+print(indice)# Vai imprimir à posição 9 que se encontra no gmail.
+
+"Outro exemplo com email"
+
+email = "felipeteixeira@gmail.com"
+indice_arroba = email.index("@")
+usuario = email[0:indice_arroba]
+print("O nome de usuário é:",usuario)
+# Será impresso então "O nome de usuário é felipeteixeira"
+
+email = "evaldowolkers@gmail.com"
+indice_arroba = email.index("@")
+indice_ponto = email.index(".")
+provedor = email [indice_arroba+1:indice_ponto]
+print("O nome do provedor é: ",provedor)
+# Será impresso "O nome do provedor é: gmail
+
+email = "joaosilva@gmail.com"
+usuario, dominio = email.split("@")
+print("Usuário:", usuario)
+print("Domínio", dominio)
+
+"Será impresso da seguinte forma então." \
+"Usuário: joao.silva"
+"Domínio: gmail.com"

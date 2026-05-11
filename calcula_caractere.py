bd88d33a5cdce0322ad246e63de3d63fa065fd50
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2026-05-11 19:05:17 -0300

    Esse é o mesmo arquivo da onde eu parei na minha outra branch (Meus projetos em Python)

---
+++ calcula_caractere.py
@@ -0,0 +1,10 @@
+texto1 = input("Informe o primeiro texto: ")
+texto2 = input("Informe o segundo texto: ")
+print("Texto 1: ", texto1)
+print("Texto 2: ", texto2)
+
+print(f"Quantidade de caracteres de '{texto1}' é: {len(texto1)}")
+print(f"Quantidade de caractres de '{texto2}' é: {len(texto2)}")
+
+print("As strings possuem à mesma quantidade de caracteres?", len(texto1) == len(texto2))
+print("As strings são iguais?" ,texto1 == texto2 )

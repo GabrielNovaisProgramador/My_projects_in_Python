2847d60a8f02ecb7ef96615bcbd6ce500d890f30
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2026-05-31 12:29:11 -0300

    Analisando como é o funcionamento do operador lógico "and" na lógica de programação, utilizando Python.

---
+++ operadores_logicos_and.py
@@ -0,0 +1,24 @@
+'Revendo como é o funcionamento do operador lógico "and"! '
+
+a = True and True
+b = True and False
+c = False and True
+d = False and False
+
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("d =", d)
+
+### ---------------------------------------------------------------------------- ###
+
+print("\n\n")
+
+''' Outro exemplo da utilização do operador lógico (and) '''
+
+a = 10
+proposicao1 = (a > 1) #True
+proposicao2 = (a == 5) #False
+
+resultado = proposicao1 and proposicao2
+print(resultado)

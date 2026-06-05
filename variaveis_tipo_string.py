8c09a7f0d76efa8cb43f4c0f94916b87e15c1ff1
GabrielNovaisProgramador <gnovaesmartins@gmail.com>
2026-06-05 07:45:30 -0300

    Vendo oque pode ser feito com váriaveis que armazena o tipo string.

---
+++ variaveis_tipo_string.py
@@ -0,0 +1,17 @@
+frase = "Aula de Python sobre strings!!!"
+tamanho_frase = len(frase)
+
+print(tamanho_frase)
+
+# ------------------------------------------------------------------------------------ #
+
+print("\n\n")
+
+curso = "Python para todos"
+print(curso[1])
+
+# ------------------------------------------------------------------------------------ #
+
+'''curso = "Python para todos" 
+print(len(curso))
+print(curso[17])'''
